b67566ac5d51e08b20131e3be35e4c930bb2b9a6
Hanran <hanrankim@gmail.com>
2018-03-02 11:16:27 -0800

    made array, function randomeMole

---
+++ find-the-mole/main.js
@@ -0,0 +1,23 @@
+
+
+var array = [
+    col = [null, null, null],
+    col = [null, null, null],
+    col = [null, null, null]
+]
+
+function randomMole(mole){
+    for(var i=0; i<array.length; i++){
+        for(var j=0;j<array[i].length; j++){
+
+        }
+        var randomCol = Math.floor(Math.random()* array.length);
+        var randomRow = Math.floor(Math.random()* array[i].length);
+        array[randomCol][randomRow]='mole'
+        $('.game_area').append('mole');        
+    }
+}
+
+
+
+
